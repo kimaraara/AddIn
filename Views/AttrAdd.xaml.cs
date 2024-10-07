d1d8ea898cfee76c3d07a95d8eb14a71f5b47a35
버블 <139833891+kimaraara@users.noreply.github.com>
2024-10-07 16:08:19 +0900

    1007

--- Views/AttrAdd.xaml.cs
+++ Views/AttrAdd.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,32 @@ namespace AddIn.Views
     /// </summary>
     public partial class AttrAdd : Window
     {
+        public ObservableCollection<PropertyData> PropertyList { get; set; }
+
         public AttrAdd()
         {
             InitializeComponent();
+
+            // 예시 데이터
+            PropertyList = new ObservableCollection<PropertyData>
+        {
+            new PropertyData { VariableName = "Width"},
+            new PropertyData { VariableName = "Height"}
+        };
+
+            PropertyListUp.ItemsSource = PropertyList;
+        }
+
+        // LoadingRow 이벤트 핸들러를 통해 순번 설정
+        private void PropertyListUp_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = (e.Row.GetIndex() + 1).ToString(); // 1부터 시작하는 순번
+        }
+
+        public class PropertyData
+        {
+            public string Sequence { get; set; } // 순번
+            public string VariableName { get; set; } // 변수명
         }
 
         private void CloseAttrAdd_Click(object sender, RoutedEventArgs e)
@@ -30,6 +54,6 @@ namespace AddIn.Views
             Close();
         }
 
-
     }
 }
+
